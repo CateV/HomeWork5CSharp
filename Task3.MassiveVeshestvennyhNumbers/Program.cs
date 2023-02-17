49859dc37bc0eb1ec71401f15b8e8457fcf38297
Kate V <katsiarynait@gmail.com>
2023-02-18 01:43:00 +0300

    готово

---
+++ Task3.MassiveVeshestvennyhNumbers/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между 
+// максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+double [] FillArray(int quantity)
+{
+    double[] massive = new double[quantity];
+    for (int index = 0; index < quantity; index++)
+    {
+        massive[index] = new Random().Next(1, 100);
+    }
+    return massive;
+}
+void PrintArray (double [] massive)
+{
+    for (int i = 0; i<massive.Length; i++)
+    {
+        System.Console.Write($"{massive [i]}\t");
+    }
+}
+double BetweenMinAndMax (double[]massive)
+{
+        double min = massive[0];
+        for (int i = 1; i<massive.Length;i++)
+        {
+            if (massive[i]<min)
+            {
+                min = massive[i];
+            }
+        }
+
+        double max = massive[0];
+        for (int i = 1; i<massive.Length;i++)
+        {
+            if (massive[i]>max)
+            {
+                max = massive[i];
+            }
+        }
+double result = max - min;
+return result;
+}
+double[] ourMassive = FillArray (10);
+PrintArray (ourMassive);
+System.Console.WriteLine();
+System.Console.WriteLine($"Разница между максимальным и минимальным элементами массива равна {BetweenMinAndMax(ourMassive)}");
